0972a056256f4f24c44be4ca82882c01728da1be
theken <k_kumagai55@yahoo.co.jp>
2023-05-07 16:40:57 +0900

    fixed Make-152-split-users.py

--- train/make_parquets/Make-152-split-users.py
+++ train/make_parquets/Make-152-split-users.py
@@ -1,6 +1,16 @@
 import pandas as pd, numpy as np
 
-df = pd.read_parquet('../../data/train_data/test.parquet',columns=['session'])
+ON_KAGGLE=False
+if ON_KAGGLE:
+    INPUT='/kaggle/input/otto-mydata/otto-mydata'
+    OUTPUT='/kaggle/working'
+    OUTPUT_MAKE_PARQUETS=OUTPUT+'/data/make_parquets'
+    import os
+    for mydir in [OUTPUT_MAKE_PARQUETS]:
+        os.makedirs(mydir, exist_ok=True)
+    df = pd.read_parquet(INPUT+'/train_data/test.parquet',columns=['session'])
+else:
+    df = pd.read_parquet('../../data/train_data/test.parquet',columns=['session'])
 df.head()
 
 df.session.nunique()
@@ -8,7 +18,11 @@ df.session.nunique()
 USR = df.session.astype('int32').unique()
 len( USR )
 
-np.save('test_user_A',USR[:len(USR)//2])
+if ON_KAGGLE:
+    np.save(OUTPUT_MAKE_PARQUETS+'/test_user_A',USR[:len(USR)//2])
+    np.save(OUTPUT_MAKE_PARQUETS+'/test_user_B',USR[len(USR)//2:])
+else:
+    np.save('test_user_A',USR[:len(USR)//2])
+    np.save('test_user_B',USR[len(USR)//2:])
 
-np.save('test_user_B',USR[len(USR)//2:])
 
